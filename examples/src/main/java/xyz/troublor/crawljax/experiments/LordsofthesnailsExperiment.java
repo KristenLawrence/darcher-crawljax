650a833eba64b02bc374bca3fa8dabd33bea81a3
Aaron <troublor@live.com>
2020-12-02 14:52:50 +0800

    lordsofthesnails experiment

--- examples/src/main/java/xyz/troublor/crawljax/experiments/LordsofthesnailsExperiment.java
+++ examples/src/main/java/xyz/troublor/crawljax/experiments/LordsofthesnailsExperiment.java
@@ -6,17 +6,24 @@ import com.crawljax.core.configuration.BrowserConfiguration;
 import com.crawljax.core.configuration.CrawlRules;
 import com.crawljax.core.configuration.CrawljaxConfiguration;
 import com.crawljax.core.configuration.InputSpecification;
+import com.crawljax.core.plugin.OnUrlLoadPlugin;
 import com.crawljax.plugins.crawloverview.CrawlOverview;
 import org.kristen.crawljax.plugins.grpc.GRPCClientPlugin;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class LordsofthesnailsExperiment extends Experiment {
 
     private static final long WAIT_TIME_AFTER_EVENT = 500;
-    private static final long WAIT_TIME_AFTER_RELOAD = 500;
+    private static final long WAIT_TIME_AFTER_RELOAD = 5000;
     private static final String DAPP_URL = "http://localhost:8888/game.html";
     private static final String DAPP_NAME = "Lordsofthesnails";
     private static int instanceId = 1;
@@ -58,12 +65,28 @@ public class LordsofthesnailsExperiment extends Experiment {
 
         InputSpecification inputSpec = new InputSpecification();
 
-
+        /* Don't click external links */
+        builder.crawlRules().dontClick("A").underXPath("/html/body/div[1]/div");
 
         builder.crawlRules().setInputSpec(inputSpec);
         builder.setBrowserConfig(
                 new BrowserConfiguration(EmbeddedBrowser.BrowserType.CHROME_EXISTING, chromeDebuggerAddress));
         builder.addPlugin(new GRPCClientPlugin(DAPP_NAME, instanceId, METAMASK_POPUP_URL, DAPP_URL, METAMASK_PASSWORD));
+
+        // plugin to wait for loading
+        builder.addPlugin((OnUrlLoadPlugin) context -> {
+            WebDriver driver = context.getBrowser().getWebDriver();
+            System.out.print("Wait for Lordsofthesnails DApp reloading...");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(d -> {
+                try {
+                    WebElement sample = driver.findElement(By.xpath("//*[@id='leadereggs']"));
+                    return !sample.getText().equals("undefined");
+                } catch (NoSuchElementException ignored) {
+                    return false;
+                }
+            });
+            System.out.println("done");
+        });
 
 
         return new CrawljaxRunner(builder.build());
