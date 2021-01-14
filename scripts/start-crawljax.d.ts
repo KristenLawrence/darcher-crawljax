0d76eb05f69317b36ff0e1f596e28348cda8fa29
Aaron <troublor@live.com>
2021-01-14 19:54:11 +0800

    :recycle: eslint

--- scripts/start-crawljax.d.ts
+++
@@ -1,2 +0,0 @@
-import { Logger } from "@darcher/helpers";
-export declare function startCrawljax(logger: Logger, chromeDebuggerAddress: string, mainClass: string, timeBudget: number, logDir?: string): Promise<void>;
